28d85dd4d6708aa7845bf47aac14a84b09508d45
Semyon Danilov <samvimes@yandex.ru>
2016-03-27 21:48:53 +0300

    Update module.cpp

--- module-example/module.cpp
+++ module-example/module.cpp
@@ -7,8 +7,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     std::string line;
-    //читаем входные данные для модуля
-    //они все будут в одной строке (пофигу на формат, хоть JSON, хоть XML, хоть plain-text)
+    //json
     std::getline(std::cin, line);
     cout << line << " friend" << endl;
     return 0;
